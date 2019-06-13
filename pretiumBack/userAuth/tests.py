1b3deb7dc3d81675f7dd21498ea3df36ff45e550
dreminm <dreminmihail@gmail.com>
2019-06-13 15:48:11 +0300

    test

--- pretiumBack/userAuth/tests.py
+++ pretiumBack/userAuth/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .views import signup
+from .models import User
+from django.http import HttpRequest, QueryDict
 
-# Create your tests here.
+
+class UserAuthTestCase(TestCase):
+
+    def createModel(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST = QueryDict.__init__(request.POST, 'email=vasyapupkin1988@gmail.com&username=Vasiliy&fullname=Vasiliy Ivanovich Pupkin&password=velosiped124', True)
+        signup(request)
+        if User.objects.filter(username='Vasiliy').exists():
+            self.assertEqual(User.objects.filter(username='Vasiliy').password, 'velosiped124')
+        elif User.objects.filter(email='vasyapupkin1988@gmail.com').exists():
+            self.assertEqual(User.objects.filter(username='Vasiliy').password, 'velosiped124')
+        else:
+            self.assertTrue(False)
